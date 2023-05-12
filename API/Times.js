c1d632506dffc161c9d43bd1a6c9fcbad9fe707d
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2023-05-11 23:45:30 -0300

    Update Times.js

--- API/Times.js
+++ API/Times.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 import useSWR from 'swr';
 
-const API_BASE_URL = "https://parseapi.back4app.com/classes"; // altere para a sua URL de API
+const API_BASE_URL = "https://parseapi.back4app.com/classes/"; // altere para a sua URL de API
+
+const instance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "X-Parse-Application-Id": "aLQLnG7jD35CFlE0VwFDPgHPkb8hXBaMOwi0vHWn",
+    "X-Parse-REST-API-Key": "HxqwhdGFlWjPwtK09DoNkjTX1wCMlO6Hkg5dSrh7",
+  },
+});
+
 
 export const fetchDados = async () => {
-  const response = await axios.get(`${API_BASE_URL}/times`, {
-    headers: {
-      "X-Parse-Application-Id": "aLQLnG7jD35CFlE0VwFDPgHPkb8hXBaMOwi0vHWn",
-      "X-Parse-REST-API-Key": "HxqwhdGFlWjPwtK09DoNkjTX1wCMlO6Hkg5dSrh7",
-    },
-  });
+  const response = await instance.get("Times");
   return response.data.results;
 };
 
